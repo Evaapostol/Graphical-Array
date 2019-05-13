216d540735f447acb594f6b6df5816391ad76b6c
Evaapostol <eugeniaapostol@hotmail.gr>
2019-05-13 05:35:47 +0300

    Add project files.

---
+++ GraphicalArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace GraphicalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 7, 4, 3 };
+            numbers = PreProcess(numbers);
+            Console.WriteLine("Enter symbol");
+            char symbol = char.Parse(Console.ReadLine());
+            Console.WriteLine("############");
+            foreach (int aNumber in numbers)
+            {
+                GraphicalRepresentation(aNumber, symbol);
+            }
+        }
+
+        static int[] PreProcess(int[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] < 0) data[i] = 0;
+                if (data[i] > 60) data[i] = 60;
+            }
+            return data;
+        }
+
+        static void GraphicalRepresentation(int figure, char sign)
+        {
+            Console.Write("{0:00}:", figure);
+            for (int i = 0; i < figure; i++)
+                Console.Write(sign);
+            Console.WriteLine();
+        }
+    }
+}
